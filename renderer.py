beaf09770a1cbae0a068b076242a2dae29c41fb9
sumegim <sumegi.mark@gmail.com>
2024-02-20 13:26:18 +0100

    template cleanup

--- renderer.py
+++ renderer.py
@@ -1,39 +1,6 @@
 import os
 import yaml
 
-template = """
-const container = document.createElement('div');
-container.classList.add('container');
-
-for (let i = 0; i < games.length; i++) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    
-    const link = document.createElement('a');
-    link.setAttribute('href', games[i].url);
-    link.classList.add('text-link'); // Add a class to the link elements
-
-    const img = document.createElement('img');
-    img.setAttribute('src', games[i].id + '/image.jpg');
-
-    tile.appendChild(img);
-    
-    const middle = document.createElement('div');
-    middle.classList.add('middle');
-
-    const text = document.createElement('div');
-    text.classList.add('text');
-    text.innerText = games[i].description;
-    link.appendChild(text)
-
-    middle.appendChild(link);
-    tile.appendChild(middle);
-    container.appendChild(tile);
-}
-
-document.body.appendChild(container);
-"""
-
 # Get all directories in the current folder
 dirs = [d for d in os.listdir('.') if os.path.isdir(d) and d != '.git' ]
 
@@ -43,8 +10,11 @@ for dir in dirs:
         g = yaml.load(f, Loader=yaml.FullLoader)
         games.append(g)
 
-data_injection = f'const games = {games}'
+lines = []
+with open('script.js', 'r') as f:
+    lines = f.readlines()
+lines[0] = f'const games = {games};\n'
 
 # Write the template to a file
 with open('script.js', 'w') as f:
-    f.write(data_injection + template)
+    f.writelines(lines)
